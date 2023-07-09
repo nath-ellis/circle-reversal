ebfc19e0ab1a722a81ddb11cdb52a6ca68a9b335
Nathan <nathanellis15782@protonmail.com>
2023-07-09 15:53:30 +0100

    Added a sound effect when a breakable box is destroyed

--- scripts/breakable_box.gd
+++ scripts/breakable_box.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
 
+@onready var hitbox = $CollisionShape2D
 @onready var selected = $Selected
+@onready var break_sfx = $Break
 
 
 func select():
@@ -13,4 +15,10 @@ func deselect():
 
 
 func break_self():
+	break_sfx.play()
+	hide()
+	hitbox.disabled = true
+
+
+func _on_break_finished():
 	queue_free()
